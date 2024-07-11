879a26f90a4ff53b44c2ae23bc78e45a8338a475
taji <taji@sonicgarden.jp>
2024-07-11 19:39:23 +0900

    uidとproviderカラムを追加する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_09_115716) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_10_002053) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,8 +22,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_09_115716) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid", default: "", null: false
+    t.string "provider", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
 end
